b7ba100ace1032ba3e2c3eebdbb468c1c4036fab
hyeonnnho <tiger1330@naver.com>
2025-03-12 09:48:53 +0900

    1일차 커밋

--- Day01/scanf3.c
+++ Day01/scanf3.c
@@ -1,4 +1,4 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 
 int main()
 {
@@ -10,4 +10,13 @@ int main()
 
 	printf("나이는 %d 이고 이름은 %s 입니다.\n", n1, str);
 	return 0;
+
+    //int n1;
+    //char str[20];
+    //printf("이름와 나이을 입력하세요 >> \n");
+    //scanf_s("%s", str, sizeof(str));
+    //scanf_s("%d", &n1);
+
+    //printf("이름은 %s 이고 나이는 %d 입니다.\n", str, n1);
+    //return 0;
 }
